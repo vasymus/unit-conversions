52bb02c9109cf39a38dc5c227519cd2216c16ca7
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2021-07-24 19:18:51 +0300

    Initial installations.

---
+++ src/UnitConversionsClass.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Vasymus\UnitConversions;
+
+class UnitConversionsClass
+{
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Vasymus\UnitConversions\Tests;
 
 use PHPUnit\Framework\TestCase;
 
