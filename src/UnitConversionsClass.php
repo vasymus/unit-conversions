03791a8b79d3ac89395693cfa872238d5b192e45
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2021-07-24 19:55:41 +0300

    converts kilograms to lbs

--- src/UnitConversionsClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Vasymus\UnitConversions;
-
-class UnitConversionsClass
-{
-}
---
+++ tests/WeightTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Vasymus\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Vasymus\UnitConversions\Weight;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_converts_kilograms_to_lgs()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+
+        $this->assertEquals(220.4623, $lbs);
+    }
+}
