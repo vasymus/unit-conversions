5e0c90d9dd07d083e4bf9b50aac85c3a083b500d
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2021-08-15 18:18:47 +0300

    add temperature conversion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `unit-conversions` will be documented in this file.
 
+## 1.1.0 - 2021-08-15
+
+- Add temperature conversion
+
 ## 1.0.0 - 2021-08-15
 
 - initial release
---
+++ tests/TemperatureTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vasymus\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Vasymus\UnitConversions\Temperature;
+use Vasymus\UnitConversions\Weight;
+
+class TemperatureTest extends TestCase
+{
+    /** @test */
+    public function it_can_converts_celsius_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::fromCelsius(100)->toFahrenheit();
+
+        $this->assertEquals(212, $fahrenheit);
+    }
+}
